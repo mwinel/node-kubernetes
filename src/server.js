55de58d2f452d293e90e8613daf7e22e7080b979
Nelson M <mwinel@live.com>
2023-12-12 17:17:50 +0300

    Fix health response message

--- src/server.js
+++ src/server.js
@@ -2,12 +2,12 @@
   imports
 */
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const knex = require('./db/knex');
+const knex = require("./db/knex");
 
 /*
   middleware
@@ -20,72 +20,93 @@ app.use(bodyParser.urlencoded({ extended: false }));
   routes
 */
 
-app.get('/', (req, res) => {
-  res.json('pong!');
+app.get("/", (req, res) => {
+  res.json("pong!");
 });
 
-app.get('/health', (req, res) => {
-  res.json('healthy!');
+app.get("/health", (req, res) => {
+  res.json({
+    status: "healthy",
+  });
 });
 
-app.get('/metrics', (req, res) => {
+app.get("/metrics", (req, res) => {
   res.json({
     uptime: process.uptime(),
     memoryUsage: process.memoryUsage(),
     version: process.version,
     platform: process.platform,
-    nodeEnv: process.env.NODE_ENV
+    nodeEnv: process.env.NODE_ENV,
   });
 });
 
-app.get('/status', (req, res) => {
+app.get("/status", (req, res) => {
   res.json({
-    status: 'running ok'
+    status: "running ok",
   });
 });
 
-app.get('/todos', (req, res) => {
-  knex('todos')
-  .then((data) => { res.json(data); })
-  .catch(() => { res.json('Something went wrong.') });
+app.get("/todos", (req, res) => {
+  knex("todos")
+    .then((data) => {
+      res.json(data);
+    })
+    .catch(() => {
+      res.json("Something went wrong.");
+    });
 });
 
-app.get('/todos/:id', (req, res) => {
-  knex('todos')
-  .where({ id:  parseInt(req.params.id) })
-  .then((data) => { res.json(data); })
-  .catch(() => { res.json('Something went wrong.') });
+app.get("/todos/:id", (req, res) => {
+  knex("todos")
+    .where({ id: parseInt(req.params.id) })
+    .then((data) => {
+      res.json(data);
+    })
+    .catch(() => {
+      res.json("Something went wrong.");
+    });
 });
 
-app.post('/todos', (req, res) => {
-  knex('todos')
-  .insert({
-    title: req.body.title,
-    completed: false
-  })
-  .then(() => { res.json('Todo added!'); })
-  .catch(() => { res.json('Something went wrong.') });
+app.post("/todos", (req, res) => {
+  knex("todos")
+    .insert({
+      title: req.body.title,
+      completed: false,
+    })
+    .then(() => {
+      res.json("Todo added!");
+    })
+    .catch(() => {
+      res.json("Something went wrong.");
+    });
 });
 
-app.put('/todos/:id', (req, res) => {
-  knex('todos')
-  .where({ id: parseInt(req.params.id) })
-  .update({
-    title: req.body.title,
-    completed: req.body.completed
-  })
-  .then(() => { res.json('Todo updated!'); })
-  .catch(() => { res.json('Something went wrong.') });
+app.put("/todos/:id", (req, res) => {
+  knex("todos")
+    .where({ id: parseInt(req.params.id) })
+    .update({
+      title: req.body.title,
+      completed: req.body.completed,
+    })
+    .then(() => {
+      res.json("Todo updated!");
+    })
+    .catch(() => {
+      res.json("Something went wrong.");
+    });
 });
 
-app.delete('/todos/:id', (req, res) => {
-  knex('todos')
-  .where({ id: parseInt(req.params.id) })
-  .del()
-  .then(() => { res.json('Todo deleted!'); })
-  .catch(() => { res.json('Something went wrong.') });
+app.delete("/todos/:id", (req, res) => {
+  knex("todos")
+    .where({ id: parseInt(req.params.id) })
+    .del()
+    .then(() => {
+      res.json("Todo deleted!");
+    })
+    .catch(() => {
+      res.json("Something went wrong.");
+    });
 });
-
 
 /*
   run server
